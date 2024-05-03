5792d1fbf7e8afb40bf08b0650194e01727bfad7
Ribesh7Pokharel <>
2024-05-03 11:56:51 +1000

    2nd commit

--- app/src/main/java/com/example/itube_app/MainActivity.java
+++ app/src/main/java/com/example/itube_app/MainActivity.java
@@ -3,36 +3,30 @@ package com.example.itube_app;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
-    private DatabaseHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dbHelper = new DatabaseHelper(this);
+        Button btnSignUp = findViewById(R.id.btnSignUp);
+        Button btnLogIn = findViewById(R.id.btnLogIn);
 
-        // Assuming a simple delay to mimic loading resources or checking credentials
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                checkLoginAndRedirect();
-            }
-        }, 2000); // Delay for 2 seconds
-    }
+        btnSignUp.setOnClickListener(v -> {
+            // Navigate to SignUpActivity when the user clicks the Sign Up button
+            Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+            startActivity(intent);
+        });
 
-    private void checkLoginAndRedirect() {
-        // Assuming 'isUserLoggedIn' is a method that checks the logged-in status from your DatabaseHelper
-        if (dbHelper.isUserLoggedIn()) {
-            startActivity(new Intent(MainActivity.this, VideoManagementActivity.class));
-        } else {
-            startActivity(new Intent(MainActivity.this, LoginActivity.class));
-        }
-        finish(); // Ensures this activity is removed from the back stack
+        btnLogIn.setOnClickListener(v -> {
+            // Navigate to LoginActivity when the user clicks the Log In button
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
     }
 }
+
 
